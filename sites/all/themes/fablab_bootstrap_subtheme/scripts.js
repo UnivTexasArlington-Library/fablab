b0f4cf3306624a86fc143dd05c57fdfb6abb8aba
elizoller <zoller.eli@gmail.com>
2014-09-30 18:38:03 -0500

    modifying js for drupal

--- sites/all/themes/fablab_bootstrap_subtheme/scripts.js
+++ sites/all/themes/fablab_bootstrap_subtheme/scripts.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
-	var winheight = window.height();
+	var winheight = jQuery(window).height();
 	jQuery(".right").css("height", winheight);
 	jQuery(".left").css("height", winheight);
 	jQuery(window).resize(function() {
-		var winheight = window.height();
+		var winheight = jQuery(window).height();
 		jQuery(".right").css("height", winheight);
 		jQuery(".left").css("height", winheight);
 	});
