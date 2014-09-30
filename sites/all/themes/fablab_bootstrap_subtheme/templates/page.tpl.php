6ff1eb9c458ff3b288c46b1f7677743ba4795b74
elizoller <zoller.eli@gmail.com>
2014-09-30 17:32:01 -0500

    removing extraneous nav from template

--- sites/all/themes/fablab_bootstrap_subtheme/templates/page.tpl.php
+++ sites/all/themes/fablab_bootstrap_subtheme/templates/page.tpl.php
@@ -73,17 +73,17 @@
  * @ingroup themeable
  */
 ?>
-<!--<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+<!--<header id="navbar" role="banner" class="<?php //print $navbar_classes; ?>">
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      <a class="logo navbar-btn pull-left" href="<?php //print $front_page; ?>" title="<?php //print t('Home'); ?>">
+        <img src="<?php //print $logo; ?>" alt="<?php //print t('Home'); ?>" />
       </a>
       <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <a class="name navbar-brand" href="<?php //print $front_page; ?>" title="<?php //print t('Home'); ?>"><?php //print $site_name; ?></a>
       <?php endif; ?>
 -->
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
@@ -139,7 +139,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+        <!--<h1 class="page-header"><?php //print $title; ?></h1>-->
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
