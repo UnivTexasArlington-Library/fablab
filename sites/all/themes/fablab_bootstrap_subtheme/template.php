f049c5bd5f49c167c5263ec138805ece63ef5cd2
elizoller <zoller.eli@gmail.com>
2014-09-30 17:00:14 -0500

    beginning to make custom styling for fablab

--- sites/all/themes/fablab_bootstrap_subtheme/template.php
+++ sites/all/themes/fablab_bootstrap_subtheme/template.php
@@ -4,3 +4,7 @@
  * @file
  * template.php
  */
+//this adds custom js
+function library_bootstrap_subtheme_preprocess_html(&$variables) {
+  drupal_add_js('http://library.uta.edu/jwplayer/jwplayer.js');
+}
