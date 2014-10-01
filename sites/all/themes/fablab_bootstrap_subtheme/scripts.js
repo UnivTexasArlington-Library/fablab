f271823c0a9ba5ab82a8fe17c6982a73e4ce6470
elizoller <zoller.eli@gmail.com>
2014-09-30 19:00:50 -0500

    styling for k12

--- sites/all/themes/fablab_bootstrap_subtheme/scripts.js
+++ sites/all/themes/fablab_bootstrap_subtheme/scripts.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
 	var winheight = jQuery(window).height();
-	jQuery(".right").css("height", winheight);
-	jQuery(".left").css("height", winheight);
+	jQuery(".main").css("min-height", winheight - 220);
+	jQuery(".left").css("min-height", winheight);
 	jQuery(window).resize(function() {
 		var winheight = jQuery(window).height();
-		jQuery(".right").css("height", winheight);
-		jQuery(".left").css("height", winheight);
+		jQuery(".main").css("min-height", winheight - 220);
+		jQuery(".left").css("min-height", winheight);
 	});
 });
