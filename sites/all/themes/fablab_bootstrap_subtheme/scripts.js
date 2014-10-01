aaad13639ad7e5fb0bd908903e76f76419462283
elizoller <zoller.eli@gmail.com>
2014-09-30 19:20:38 -0500

    modifying leftright height

--- sites/all/themes/fablab_bootstrap_subtheme/scripts.js
+++ sites/all/themes/fablab_bootstrap_subtheme/scripts.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
 	var winheight = jQuery(window).height();
-	jQuery(".main").css("min-height", winheight - 220);
+	jQuery(".main").css("min-height", winheight - 275);
 	jQuery(".left").css("min-height", winheight);
 	jQuery(window).resize(function() {
 		var winheight = jQuery(window).height();
-		jQuery(".main").css("min-height", winheight - 220);
+		jQuery(".main").css("min-height", winheight - 275);
 		jQuery(".left").css("min-height", winheight);
 	});
 });
