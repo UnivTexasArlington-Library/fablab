78b072f9a4eea8dc3263792a8448be05a345e456
Eli <zoller.eli@gmail.com>
2014-10-09 11:39:58 -0400

    fixing js

--- sites/all/themes/fablab_bootstrap_subtheme/scripts.js
+++ sites/all/themes/fablab_bootstrap_subtheme/scripts.js
@@ -1,20 +1,11 @@
 jQuery(document).ready(function() {
 	var winheight = jQuery(window).height();
-<<<<<<< HEAD
 	jQuery(".main").css("min-height", winheight - 222);
 	jQuery(".left").css("min-height", winheight);
 	jQuery(window).resize(function() {
 		var winheight = jQuery(window).height();
 		jQuery(".main").css("min-height", winheight - 222);
 		jQuery(".left").css("min-height", winheight);
-=======
-	jQuery(".main").css("height", winheight - 275);
-	jQuery(".left").css("height", winheight);
-	jQuery(window).resize(function() {
-		var winheight = jQuery(window).height();
-		jQuery(".main").css("height", winheight - 275);
-		jQuery(".left").css("height", winheight);
->>>>>>> 18c9c2c70cf245dff79a05396f2bce7edb393b45
 	});
 	jQuery(".logo:first").mouseenter(function() {
 		jQuery(".logo").toggle();
