38ed332fe7dbc5b647e63a69e278e1c77ed3925b
kschenk <kschenk@uta.edu>
2015-05-05 15:14:36 -0500

    header nav links

--- sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
+++ sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
@@ -74,7 +74,7 @@
  */
 ?>
 <header id="navbar" role="banner" class="navbar container-fluid navbar-default">
-  <div class="container">
+  <div class="container-fluid">
     <div class="navbar-header">
       <?php if ($logo): ?>
       <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
