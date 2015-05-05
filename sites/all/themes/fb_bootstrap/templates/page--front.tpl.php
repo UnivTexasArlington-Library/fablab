79b7c99dc4cf6efa18bc77b4964990a4ecc07693
kschenk <kschenk@uta.edu>
2015-05-05 16:18:08 -0500

    css & font page templage changes

--- sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
+++ sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
@@ -113,58 +113,41 @@
   </div>
 </header>
 
-<div class="main-container container-fluid">
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </section>
-
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
+<body>
+  <?php print render($title_prefix); ?>
+  <?php if (!empty($title)): ?>
+  <!--  <h1 class="page-header"><?php print $title; ?></h1> -->
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php print $messages; ?>
+  <?php if (!empty($tabs)): ?>
+  <?php print render($tabs); ?>
+  <?php endif; ?>
+  <?php if (!empty($page['help'])): ?>
+  <?php print render($page['help']); ?>
+  <?php endif; ?>
+  <?php if (!empty($action_links)): ?>
+  <ul class="action-links"><?php print render($action_links); ?></ul>
+  <?php endif; ?>
     
+    <!-- JUMBOTRON/HIGHLIGHTED -->
+    
+  <div class="jumbotron">
+    <div class="container" > <!--BACKGROUND IMAGE HERE -->
+      
+        <?php if (!empty($page['highlighted'])): ?>
+        
+          <?php print render($page['highlighted']); ?>
+        
+  
+    </div> <!-- /. container -->
+  </div> <!-- /.jumbotron -->
+  <?php endif; ?>
           
     <!--LEFT/MIDDLE/RIGHT-->
 
-  <div class="container-fluid" id="grey-scale">
-    <div class="row">
+  <div class="container-fluid">
+    <div class="row" id="grey-scale">
       <div class="col-md-4 col-sm-4">
         <?php print render($page['left']); ?>
       </div> <!-- /col-4 -->
@@ -178,8 +161,8 @@
       </div> <!-- /col-4 -->
     </div> <!-- /.row -->
   </div> <!-- /.container-fluid (END OF LEFT/MIDDLE/RIGHT) -->
-<?php endif; ?>
-</div>
+
+</body>
 <footer class="footer container-fluid">
   <?php print render($page['footer']); ?>
 </footer>
