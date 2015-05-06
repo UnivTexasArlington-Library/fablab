928c91e80624b6c5dfef10b534670283517322a6
kschenk <kschenk@uta.edu>
2015-05-06 18:35:45 -0500

    Big Ideas Block CSS

--- sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
+++ sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
@@ -81,13 +81,9 @@
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+    
+    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -96,19 +92,20 @@
     </div>
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
+    <div class="navbar-collapse collapse">
+      <nav role="navigation">
+        <?php if (!empty($primary_nav)): ?>
+          <?php print render($primary_nav); ?>
+        <?php endif; ?>
+        <?php if (!empty($secondary_nav)): ?>
+          <?php print render($secondary_nav); ?>
+        <?php endif; ?>
+        <div class="container-fluid" > <!--BACKGROUND IMAGE HERE -->
+        <?php if (!empty($page['navigation'])): ?>
+          <?php print render($page['navigation']); ?>
+        <?php endif; ?>
+      </nav>
+    </div>
     <?php endif; ?>
   </div>
 </header>
@@ -133,14 +130,11 @@
     <!-- JUMBOTRON/HIGHLIGHTED -->
     
   <div class="jumbotron">
-    <div class="container-fluid" > <!--BACKGROUND IMAGE HERE -->
-      
-        <?php if (!empty($page['highlighted'])): ?>
+    <div class="container-fluid">   
+          <?php if (!empty($page['highlighted'])): ?>
         
           <?php print render($page['highlighted']); ?>
-        
-  
-    </div> <!-- /. container -->
+    </div> <!-- /. container-fluid -->
   </div> <!-- /.jumbotron -->
   <?php endif; ?>
           
