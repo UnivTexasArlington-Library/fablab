43966562bc95c0c390b580e610b1eddeb76a782e
kschenk <kschenk@uta.edu>
2015-07-28 08:25:37 -0500

    primary tabs take 2

--- sites/all/themes/fb_bootstrap/page.tpl.php
+++ sites/all/themes/fb_bootstrap/page.tpl.php
@@ -117,9 +117,9 @@
           <?php endif; ?>
           <?php print render($title_suffix); ?>
           <?php print $messages; ?>
-          <?php if (!empty($tabs)): ?>
+          <!--<?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
-          <?php endif; ?>
+          <?php endif; ?>-->
           <?php if (!empty($page['help'])): ?>
           <?php print render($page['help']); ?>
           <?php endif; ?>
