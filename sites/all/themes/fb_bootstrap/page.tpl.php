a8889af09000c06ff5ec0bec8e37ddb10f4c607f
kschenk <kschenk@uta.edu>
2015-07-28 08:23:28 -0500

    The real original page.tpl

--- sites/all/themes/fb_bootstrap/page.tpl.php
+++ sites/all/themes/fb_bootstrap/page.tpl.php
@@ -73,21 +73,17 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
+<header id="navbar" role="banner" class="navbar container-fluid navbar-default">
+  <div class="container-fluid">
     <div class="navbar-header">
       <?php if ($logo): ?>
       <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
       <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+    
+    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -96,24 +92,49 @@
     </div>
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
+    <div class="navbar-collapse collapse">
+      <nav role="navigation">
+        <?php if (!empty($primary_nav)): ?>
+          <?php print render($primary_nav); ?>
+        <?php endif; ?>
+        <?php if (!empty($secondary_nav)): ?>
+          <?php print render($secondary_nav); ?>
+        <?php endif; ?>
+        <div class="container-fluid" > <!--BACKGROUND IMAGE HERE -->
+        <?php if (!empty($page['navigation'])): ?>
+          <?php print render($page['navigation']); ?>
+        <?php endif; ?>
+      </nav>
+    </div>
     <?php endif; ?>
   </div>
 </header>
+<body>
+    <div class="container-fluid">   
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+          <!--  <h1 class="page-header"><?php print $title; ?></h1> -->
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+          <?php endif; ?>
+          <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
 
-<div class="main-container container">
+      <?php if (!empty($page['highlighted'])): ?>
+    
+      <?php print render($page['highlighted']); ?>
+
+    </div> <!-- /. container-fluid -->
+  <?php endif; ?>
+          
+<div id="grey-scale" class="main-container container-fluid">
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
@@ -163,6 +184,7 @@
 
   </div>
 </div>
-<footer class="footer container">
+</body>
+<footer class="footer container-fluid">
   <?php print render($page['footer']); ?>
 </footer>
