d5cdeab1254a93b1be74d702ff1fb96ca9412e0a
kschenk <kschenk@uta.edu>
2015-07-29 17:04:17 -0500

    basic page template
    offset main content

--- sites/all/themes/fb_bootstrap/page.tpl.php
+++ sites/all/themes/fb_bootstrap/page.tpl.php
@@ -110,80 +110,51 @@
   </div>
 </header>
 <body>
-    <div class="container-fluid">   
-          <?php print render($title_prefix); ?>
-          <?php if (!empty($title)): ?>
-          <!--  <h1 class="page-header"><?php print $title; ?></h1> -->
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php print $messages; ?>
-          <!--<?php if (!empty($tabs)): ?>
-          <?php print render($tabs); ?>
-          <?php endif; ?>-->
-          <?php if (!empty($page['help'])): ?>
-          <?php print render($page['help']); ?>
-          <?php endif; ?>
-          <?php if (!empty($action_links)): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-          <?php endif; ?>
+  <div class="main-container container-fluid">
 
-      <?php if (!empty($page['highlighted'])): ?>
-    
-      <?php print render($page['highlighted']); ?>
-
-    </div> <!-- /. container-fluid -->
-  <?php endif; ?>
-          
-<div id="grey-scale" class="main-container container-fluid">
-
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#page-header -->
-
-  <div class="row">
-
-    <?php if (!empty($page['sidebar_first'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>
-
-    <section<?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+    <section id="slogan">
+      <?php if (!empty($site_slogan)): ?>
+      <h3>?php print $site_slogan; ?></p>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
     </section>
 
-    <?php if (!empty($page['sidebar_second'])): ?>
-      <aside class="col-sm-3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
+    <section class="row">
+       <?php if (!empty($page['highlighted'])): ?>
+          <div class="highlighted"><?php print render($page['highlighted']); ?></div>
+        <?php endif; ?>
+        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if (!empty($title)): ?>
+          <h1 class="page-header"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php if (!empty($tabs)): ?>
+          <?php print render($tabs); ?>
+        <?php endif; ?>
+        <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+        <?php endif; ?>
+        <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+    </section>
 
+    <section class="row">
+
+      <section class="col-sm-offset-2 col-sm-5">
+        <?php print render($page['content']); ?>
+      </section>
+
+      <?php if (!empty($page['sidebar_second'])): ?>
+        <aside class="col-sm-offset-2 col-sm-2 " role="complementary">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+
+    </section>
   </div>
-</div>
 </body>
 <footer class="footer container-fluid">
   <?php print render($page['footer']); ?>
