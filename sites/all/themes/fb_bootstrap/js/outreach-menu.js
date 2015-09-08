546f225361b479af35c722355a933977648dddea
Priyadharshini132 <priya.guns@gmail.com>
2015-09-08 11:03:13 -0500

    JS check on Outreach Menu

--- sites/all/themes/fb_bootstrap/js/outreach-menu.js
+++ sites/all/themes/fb_bootstrap/js/outreach-menu.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	
 	//add class to remove padding
 	$("aside ul li a").on("click", function(e) {
-		
+		alert(this);
 		$(this).addClass("remove-border");
 		$(this).addClass("remove-padding-bottom");	
 		
