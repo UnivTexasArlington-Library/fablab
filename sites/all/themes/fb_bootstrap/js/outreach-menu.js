8713961a34c8df1ad18f4ded7e6fa75f180da812
Priyadharshini132 <priya.guns@gmail.com>
2015-09-09 12:17:33 -0500

    Final Update

--- sites/all/themes/fb_bootstrap/js/outreach-menu.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-	
-	//add class to remove padding
-	$("aside ul li a").on("click", function(e) {
-		alert("OUTREACH");
-		$(this).addClass("remove-border");
-		$(this).addClass("remove-padding-bottom");	
-		
-	});
