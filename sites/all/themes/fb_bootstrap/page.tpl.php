647701ceceb62206a98608ce1d959afdfeed1050
kschenk <kschenk@uta.edu>
2015-09-18 09:47:53 -0500

    responsive logo

--- sites/all/themes/fb_bootstrap/page.tpl.php
+++ sites/all/themes/fb_bootstrap/page.tpl.php
@@ -151,7 +151,7 @@
 
     <div class="row">
 
-      <div class="col-sm-offset-2 col-sm-6 col-xs-10 col-xs-offset-1">
+      <div class="col-sm-offset-2 col-sm-7 col-xs-10 col-xs-offset-1">
         <?php print render($page['content']); ?>
       </div>
 
