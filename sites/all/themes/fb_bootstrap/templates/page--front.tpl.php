5eb192dfce8213ca00dd89cb3b09cc78406561c9
Krystal Schenk <kschenk@uta.edu>
2016-04-05 11:39:07 -0500

    fb app script

--- sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
+++ sites/all/themes/fb_bootstrap/templates/page--front.tpl.php
@@ -75,6 +75,23 @@
  * @ingroup themeable
  */
 ?>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1671506436432957',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <div class="jumbotron">  
   <!-- JUMBOTRON/HIGHLIGHTED -->
   <div class="container-fluid">   
